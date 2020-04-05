fa237cfe0b9e74226990345c50906bde9e331013
Alexander Petrushin <stuartrevol@gmail.com>
2020-04-06 01:54:48 +0300

    Moved nodes and weights to separate file

--- gaussKronrod.py
+++ gaussKronrod.py
@@ -1,56 +1,26 @@
-import bisect
+from consts import gausskronrod_nodes, gauss_weights, kronrod_weights
+from scipy.integrate import quad
 from math import cos, sin, sqrt
 import numpy as np
-from scipy.integrate import quad
+import bisect
 
-# узлы и веса для Гаусса-Кронрода
-gausskronrod_nodes = np.array([
-    0.949107912342759, -0.949107912342759,
-    0.741531185599394, -0.741531185599394,
-    0.405845151377397, -0.405845151377397,
-    0.000000000000000,
-    0.991455371120813, -0.991455371120813,
-    0.864864423359769, -0.864864423359769,
-    0.586087235467691, -0.586087235467691,
-    0.207784955007898, -0.207784955007898
-])
-
-gauss_weights = np.array([
-    0.129484966168870, 0.129484966168870,
-    0.279705391489277, 0.279705391489277,
-    0.381830050505119, 0.381830050505119,
-    0.417959183673469
-])
-
-kronrod_weights = np.array([
-    0.063092092629979, 0.063092092629979,
-    0.140653259715525, 0.140653259715525,
-    0.190350578064785, 0.190350578064785,
-    0.209482141084728,
-    0.022935322010529, 0.022935322010529,
-    0.104790010322250, 0.104790010322250,
-    0.169004726639267, 0.169004726639267,
-    0.204432940075298, 0.204432940075298
-])
-
-
-def integrate_gausskronrod(f, a, b, args=()):
+def integrate_gausskronrod(f, a, b, nodes, args=()):
     assert b > a
 
     mid = 0.5 * (b + a)
     dx = 0.5 * (b - a)
-    zi = mid + gausskronrod_nodes * dx
+    zi = mid + gausskronrod_nodes[nodes] * dx
 
     integrand = f(zi)
-    integral_G7 = np.sum(integrand[:7] * gauss_weights)
-    integral_K15 = np.sum(integrand * kronrod_weights)
+    integral_G7 = np.sum(integrand[:7] * gauss_weights[nodes])
+    integral_K15 = np.sum(integrand * kronrod_weights[nodes])
 
     error = (200 * abs(integral_G7 - integral_K15)) ** 1.5
 
     return integral_K15 * dx, dx * error
 
 
-def integrate(f, a, b, args=(), minintervals=1, limit=200, tol=1e-10):
+def integrate(f, a, b, nodes, args=(), minintervals=1, limit=200, tol=1e-10):
     fv = np.vectorize(f)
 
     intervals = []
@@ -58,7 +28,7 @@ def integrate(f, a, b, args=(), minintervals=1, limit=200, tol=1e-10):
     limits = np.linspace(a, b, minintervals + 1)
 
     for left, right in zip(limits[:-1], limits[1:]):
-        I, err = integrate_gausskronrod(fv, left, right, args)
+        I, err = integrate_gausskronrod(fv, left, right, nodes, args)
         bisect.insort(intervals, (err, left, right, I))
 
     while True:
@@ -79,9 +49,9 @@ def integrate(f, a, b, args=(), minintervals=1, limit=200, tol=1e-10):
         mid = left + (right - left) / 2
 
         # вычисляем интегралы и ошибки, заменяем один элемент в списке и добавляем другой в конец
-        I, err = integrate_gausskronrod(fv, left, mid, args)
+        I, err = integrate_gausskronrod(fv, left, mid, nodes, args)
         bisect.insort(intervals, (err, left, mid, I))
-        I, err = integrate_gausskronrod(fv, mid, right, args)
+        I, err = integrate_gausskronrod(fv, mid, right, nodes, args)
         bisect.insort(intervals, (err, mid, right, I))
 
 
@@ -90,8 +60,9 @@ if __name__ == "__main__":
     f = lambda x: x * sin(p * x)
     g = lambda x: -x / p * cos(p * x) + 1 / p ** 2 * sin(p * x)
     a, b = 1, 4
+    nodes = 15
 
     expected = g(b) - g(a)
 
-    for result, esterror in (quad(f, a, b), integrate(f, a, b)):
+    for result, esterror in (quad(f, a, b), integrate(f, a, b, nodes)):
         print("{:15.13f} {:15g} {:15g}".format(result, esterror, 1 - result / expected))
